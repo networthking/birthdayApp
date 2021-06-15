a7cce51d416dd3ea5f599a84b79251117b55404d
NetWorthKing <j.abenchuchan@gmail.com>
2021-06-15 02:32:10 -0400

    datetime work

--- birthdayApp.py
+++ birthdayApp.py
@@ -4,14 +4,21 @@ i = 1
 while i > 0: 
 
 	month = input("Well hello, " + username + ", please tell us your birthday, starting with the month: ")
+	m = int(month)
 	day = input("What day were you born?: ")
+	d = int(day)
 	year = input("And finally! What year did you grace us?: ")
+	y = int(year)
 	verify = input("So your birthday is, " + month + " " + day + " " + year + ". Is that correct? yes / no :" )
 	if(verify == "yes"):
 		print("Great!")
 		break
 	else:
 		print("Oh no! Lets start again! ")
-date = input("whats the date?")
-
-
+import datetime
+x = int(datetime.datetime(y, m, d))
+a = (x.strftime("%j"))
+y = int(datetime.datetime.now())
+b = (y.strftime("%j"))
+c = (a - b)
+print ("Your birthday is in " + c + " days! Congrats!")
