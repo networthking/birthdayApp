40f1c388b84c766a2193727e191bb6c873f0904b
Ashot Nalbandyan <20709744+AshotAN@users.noreply.github.com>
2021-06-16 01:43:42 -0400

    Update birthdayApp.py

--- birthdayApp.py
+++ birthdayApp.py
@@ -1,3 +1,37 @@
+class App():
+	name = None
+	day = None
+	month = None
+	year = None
+	verify = None
+	
+	
+app = App()
+
+print("Welcome to the birthday countdown app. Lets see how many days is left until your birthday!")
+app.name = input("Please tell us your name:")
+
+i = 1
+while i > 0: 
+
+	app.month = int(input("Well hello, " + username + ", please tell us your birthday, starting with the month: "))
+	app.day = int(input("What day were you born?: "))
+	app.year = int(input("And finally! What year did you grace us?: "))
+	app.verify = input("So your birthday is, " + month + " " + day + " " + year + ". Is that correct? yes / no :" )
+	
+	if(app.verify == "yes"):
+		print("Great!")
+		break
+	else:
+		print("Oh no! Lets start again! ")
+		
+		
+		
+		
+		
+		
+		
+		
 class comingOfageClass():
 	test = 'hello'
 	pass
